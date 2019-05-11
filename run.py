81748cef9288c687ff27462da09bb4d53486f04c
Christine <you@example.com>
2019-05-11 13:38:32 +0300

    cretes a check existing user function

--- run.py
+++ run.py
@@ -21,3 +21,10 @@ def find_user(user_name):
     Function that finds a user by username and returns the user
     '''
     return User.find_by_user_name(user_name)
+
+
+def check_existing_users(user_name):
+    '''
+    Function that check if a user exists with that username and return a Boolean
+    '''
+    return User.user_exist(user_name)
