5ec3cb2e7a7711dd529ee601ae912ca5892f0528
Christine <you@example.com>
2019-05-11 11:58:40 +0300

    adds save code

--- user.py
+++ user.py
@@ -8,3 +8,13 @@ class User:
         self.user_name = user_name
         self.password = password
         
+
+    user_list = [] 
+
+
+    def save_user(self):
+        '''
+        save_user method saves user objects into user_list
+        '''
+
+        User.user_list.append(self)
