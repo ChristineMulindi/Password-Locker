40348e4ad39d0feedf27c44456ef5214a358a7aa
Christine <you@example.com>
2019-05-11 13:34:50 +0300

    creates a fuction to find user

--- run.py
+++ run.py
@@ -14,3 +14,10 @@ def save_users(user):
     Function to save user
     '''
     user.save_user()
+
+
+def find_user(user_name):
+    '''
+    Function that finds a user by username and returns the user
+    '''
+    return User.find_by_user_name(user_name)
